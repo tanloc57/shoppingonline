08dcd3cb7c87e503efc2184f9c79348631f5301d
tanloc57 <locnguyen5307@gmail.com>
2024-07-16 10:37:36 +0700

    Add files via upload

---
+++ client-customer/src/App.js
@@ -0,0 +1,22 @@
+//CLI: npm install axios --save
+import axios from 'axios';
+import React, { Component } from 'react';
+import './App.css';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import MyProvider from './contexts/MyProvider';
+
+
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <BrowserRouter>
+          <Main />
+        </BrowserRouter>
+      </MyProvider>
+    );
+  }
+}
+
+export default App;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: '6toescluster.ebq6xeu.mongodb.net',
+    DB_USER: 'tanloc',
+    DB_PASS: 'tanloc57',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '<jwt_secret>',
+    JWT_EXPIRES: '10000000000000000000000000000000', // in milliseconds
+    EMAIL_USER: 'locnguyen5307@gmail.com', // gmail service
+    EMAIL_PASS: 'hwfd kkey ccrp yhfr'
+};
+module.exports = MyConstants;
